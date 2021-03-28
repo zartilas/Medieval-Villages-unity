134e8f163081a8422e535d8b32c442069bf9650d
zartilas <36131492+zartilas@users.noreply.github.com>
2021-03-28 16:47:09 +0300

    Imported Assets for Doors

---
+++ Assets/Scripts/Door/DoorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject UIOpenDoor;
+    private bool doorMoving = false, doorIsOpen = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        UIOpenDoor.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        UIOpenDoor.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !doorMoving)
+        {
+            if (doorIsOpen)
+            {
+                animator.SetTrigger("Close");
+                doorMoving = true;
+                doorIsOpen = false;
+                Invoke("DoorMoving",2.2f);
+            }
+            else
+            {
+                animator.SetTrigger("Open");
+                doorMoving = true;
+                doorIsOpen = true;
+                Invoke("DoorMoving",2.2f);
+            }
+        }
+    }
+
+    void DoorMoving()
+    {
+        doorMoving = false;
+    }
+}
